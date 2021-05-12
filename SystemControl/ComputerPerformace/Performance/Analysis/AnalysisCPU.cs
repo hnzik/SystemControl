1cf471dc956910a103f96e05d2c676bcaaa6adb2
hnzik <41236278+hnzik@users.noreply.github.com>
2021-05-12 08:06:00 +0200

    Analysis

---
+++ SystemControl/ComputerPerformace/Performance/Analysis/AnalysisCPU.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemControl.ComputerPerformace.Performance.Analysis
+{
+    public partial class AnalysisCPU : UserControl
+    {
+        public AnalysisCPU()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SystemControl/ComputerPerformace/Performance/Analysis/AnalysisMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemControl.SystemSettings;
+
+namespace SystemControl.ComputerPerformace.Performance.Analysis
+{
+    class AnalysisMain : UniversalSettingsGui
+    {
+        public AnalysisMain(int code) : base(code) { }
+
+
+
+
+    }
+}
